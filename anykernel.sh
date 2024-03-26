970a10930431beb85cfb36a21f413b92abaafbee
Github <eraselk@android-dev>
2024-03-26 19:38:34 +0800

    Initial Commit

---
+++ anykernel.sh
@@ -0,0 +1,45 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=A Kernel for pascal devices.
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name=RMX2180
+device.name2=RMX2185
+device.name3=RMX2189
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+
+# end ramdisk changes
+
+write_boot;
+## end install
